2a5e4f1494d71b84536a1db1987837a9f4aac52e
Qinastha <146641022+Qinastha@users.noreply.github.com>
2024-08-18 19:52:39 +0300

    Fixing

--- rollup.config.js
+++ rollup.config.js
@@ -2,47 +2,58 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
+import {terser} from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
+import postcsss from "postcss-import"
+import postcssenv from "postcss-preset-env"
 import {dts} from "rollup-plugin-dts";
 
-const packageJson = require("./package.json");
 
 export default [
     {
         input: "src/index.ts",
         output: [
             {
-                file: packageJson.main,
+                file: "dist/index.cjs.js",
                 format: "cjs",
-                sourcemap: true,
+                sourcemap: "inline",
             },
             {
-                file: packageJson.module,
+                file: "dist/index.esm.js",
                 format: "esm",
-                sourcemap: true,
+                sourcemap: "inline",
             },
         ],
         plugins: [
+            peerDepsExternal(),
+            json(),
+            babel({
+                exclude: 'node_modules/**',
+                presets: ['@babel/preset-react'],
+                babelHelpers: 'bundled',
+            }),
             resolve(),
             commonjs(),
             postcss({
                 extract: true,
                 plugins: [
-                    require('postcss-import'),
-                    require('postcss-preset-env')({
+                    postcsss(),
+                    postcssenv({
                         stage: 0,
                     }),
                 ],
                 extensions: ['.css', '.scss'],
                 use: ['sass'],
             }),
-            typescript({tsconfig: "./tsconfig.json"}),
+            typescript({
+                tsconfig: "./tsconfig.json",
+                sourceMap: true,
+                inlineSources: true,
+            }),
+            terser(),
         ],
         external: ["react", "react-dom"],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{file: "dist/index.d.ts", format: "esm"}],
-        plugins: [dts()],
-        external: [/\.scss$/]
     },
 ];

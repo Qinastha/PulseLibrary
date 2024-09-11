42ba24c98e2b098ff5caac7af24b3a72596d4a0a
Qinastha <146641022+Qinastha@users.noreply.github.com>
2024-09-11 19:19:58 +0300

    Added new custom hook and variables

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export {default as useDebounce} from "../hooks/useDebounce";
 export {default as useViewport} from "../hooks/useViewport";
+export {default as useHideNav} from "../hooks/useHideNav";
---
+++ src/hooks/useHideNav/useHideNav.tsx
@@ -0,0 +1,40 @@
+import React, {useRef} from "react";
+
+interface UseHideNavProps {
+    onHide: () => void;
+    onShow: () => void;
+}
+
+const useHideNav = ({onHide, onShow}: UseHideNavProps) => {
+    const touchStartX = useRef(0);
+    const touchEndX = useRef(0);
+
+    const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+        touchStartX.current = e.touches[0].clientX;
+    };
+
+    const handleTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+        if (touchStartX.current - touchEndX.current > 50) {
+            onHide();
+        }
+
+        if (touchEndX.current - touchStartX.current > 50) {
+            onShow();
+        }
+
+        touchStartX.current = 0;
+        touchEndX.current = 0;
+    };
+
+    const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+        touchEndX.current = e.touches[0].clientX;
+    };
+
+    return {
+        handleTouchStart,
+        handleTouchEnd,
+        handleTouchMove,
+    };
+};
+
+export default useHideNav;
